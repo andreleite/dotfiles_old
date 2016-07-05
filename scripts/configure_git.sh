514d7202d611a7872aecd90bb58cc9f93ba920d0
André Leite <andre@andre.io>
2016-07-04 23:15:24 -0400

    Adjusting setup.sh

--- scripts/configure_git.sh
+++ scripts/configure_git.sh
@@ -4,9 +4,9 @@ git config --global color.ui true
 git config --global user.name 'André Leite'
 git config --global user.email andre@andre.io
 git config --global push.default simple
+git config --global push.followTags true
 git config --global diff.tool vimdiff
 git config --global merge.tool vimdiff
 git config --global difftool.prompt false
 git config --global core.excludesfile '~/.gitignore_global'
-git config --global push.followTags true
 git config --global http.sslVerify false
