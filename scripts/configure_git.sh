e30e591b7d445870c05e862bc29f1f17f16b882b
Andre Leite <aleite@thoughtworks.com>
2017-02-05 14:22:13 -0300

    Adjusts various

--- scripts/configure_git.sh
+++ scripts/configure_git.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 git config --global color.ui true
-git config --global user.name 'André Leite'
-git config --global user.email andre@andre.io
+git config --global user.name 'Andre Leite'
+git config --global user.email aleite@thoughtworks.com
 git config --global push.default simple
 git config --global push.followTags true
 git config --global diff.tool vimdiff
